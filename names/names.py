450e16e92b58c52e6b2b7744a5f3e27cf341e066
MurphDirt879 <boxert879@hotmail.com>
2020-05-15 11:42:45 -0400

    Names MVP

--- names/names.py
+++ names/names.py
@@ -2,6 +2,8 @@ import time
 
 start_time = time.time()
 
+from BinarySearchTree import BinarySearchTree
+
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
@@ -13,10 +15,14 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+searchTree = BinarySearchTree("") 
+
+for name in names_1:
+    searchTree.insert(name)
+for name in names_2: 
+    if searchTree.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
