288ea8fbb7e0e17985051817e1b13fc1e8da9368
MurphDirt879 <boxert879@hotmail.com>
2020-05-15 12:39:00 -0400

    RingBuffer MVP

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,28 @@
+from doublyLinkedList import DoublyLinkedList
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.firstIn = None
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.capacity > self.storage.length:
+            self.storage.add_to_tail(item)
+            if self.storage.length == 1:
+                self.firstIn = self.storage.head
+
+        else:
+            self.firstIn.value = item
+            if self.firstIn != self.storage.tail:
+                self.firstIn = self.firstIn.next
+            else:
+                self.firstIn = self.storage.head
 
     def get(self):
-        pass
+        ringBufferList = []
+        node = self.storage.head
+        while node  != None:
+            ringBufferList.append(node.value)
+            node = node.next
+        return ringBufferList
